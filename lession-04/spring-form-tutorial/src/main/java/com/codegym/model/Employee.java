8f959d8b6930788ef82f93f044dba0066648b2aa
Việt Vũ <shuhari0123@gmail.com>
2024-01-01 16:40:05 +0700

    thuchanh

---
+++ lession-04/spring-form-tutorial/src/main/java/com/codegym/model/Employee.java
@@ -0,0 +1,40 @@
+package com.codegym.model;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String contactNumber;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
---
+++ lession-04/spring-form-tutorial/src/main/webapp/WEB-INF/views/employee/info.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 1/1/2024
+  Time: 4:22 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Information</title>
+</head>
+<body>
+<h2>Submitted Employee Information</h2>
+<table>
+    <tr>
+        <td>Name:</td>
+        <td>${name}</td>
+    </tr>
+
+    <tr>
+        <td>Contact Number:</td>
+        <td>${contactNumber}</td>
+    </tr>
+
+    <tr>
+        <td>Id:</td>
+        <td>${id}</td>
+    </tr>
+</table>
+</body>
+</html>
